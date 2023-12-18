fe1d7cbdbbd87cf8a71e2d8857d2b3af31f6b64f
Denise Rezende <denise.rezende@gmail.com>
2023-12-17 23:16:30 -0300

    Nova versão

--- main.py
+++ main.py
@@ -1,7 +1,3 @@
-try:
-    from PIL import Image
-except ImportError:
-    import Image
 import os
 import cv2
 import re
@@ -48,42 +44,83 @@ def pre_processing(imagem):
     
     return blur
 
-# 2 e Z
-# 1 e I
-# Q e O
-# def replace_commun_letters(char):
-#     if char == '2':
-#         return 'Z'
-#     if char == 'Z':
-#         return '2'
-#     if char == '1':
-#         return 'I'
+def find_valid_plates(possible_plates):
+    valid_plates = []
 
-# def get_possible_plates(text):
-#     # Verificar se o texto tem pelo menos 7 caracteres
-#     if len(text) < 7:
-#         return []
+    for plate in possible_plates:
+        # Inicializa os índices
+        idx = 0
+        # Encontra as primeiras 3 letras
+        first_three_letters = ''
+        while idx < len(plate) and len(first_three_letters) < 3:
+            if plate[idx].isalpha():
+                first_three_letters += plate[idx]
+            idx += 1
 
-#     # RIOZAI94
-#     # RIO2A19
-#     possible_plates = []
+        # Encontra o próximo número
+        next_digit = ''
+        while idx < len(plate) and not plate[idx].isdigit():
+            idx += 1
+        while idx < len(plate) and plate[idx].isdigit() and len(next_digit) < 1:
+            next_digit += plate[idx]
+            idx += 1
 
-#     # Padrão: 3 letras seguidas de 4 números
-#     # 3 Letras
-#     possible_letters = text[:3]
-#     if not possible_letters.isalpha():
-#         result = ""
-#         for letter in possible_letters:
-#                 if not letter.isalpha():
-#                     result += replace_commun_letters(letter)
-#                 else:
-#                     result += letter
-    
-#     possible_numbers = text[2:]
-                
-#     # Padrão: 3 letras, 1 dígito, 1 letra, 2 dígitos
+        # Encontra a próxima letra ou numero
+        next_character = ''
+        while idx < len(plate) and not plate[idx].isalnum():
+            idx += 1
+        while idx < len(plate) and plate[idx].isalnum() and len(next_character) < 1:
+            next_character += plate[idx]
+            idx += 1
 
-#     return possible_plates
+        # Encontra os próximos dois números
+        next_two_digits = ''
+        while idx < len(plate) and not plate[idx].isdigit():
+            idx += 1
+        while idx < len(plate) and plate[idx].isdigit() and len(next_two_digits) < 2:
+            next_two_digits += plate[idx]
+            idx += 1
+
+        
+        
+        # Verifica se a placa atende aos critérios
+        if (
+            len(first_three_letters) == 3 and
+            len(next_digit) == 1 and
+            len(next_character) == 1 and
+            len(next_two_digits) == 2
+        ):
+            new_plate = f"{first_three_letters}{next_digit}{next_character}{next_two_digits}"
+            if new_plate not in valid_plates:
+                valid_plates.append(new_plate)
+
+    return valid_plates
+
+def generate_possible_plates(plate_text, max_substitutions=2):
+    substitutions = {
+        '2': ['Z'],
+        'Z': ['2'],
+        '1': ['I'],
+        'I': ['1'], 
+        'O': ['Q', '0'],
+        'Q': ['O'],
+        'G': ['4'],
+        '4': ['G'],
+    }
+
+    possible_plates = [plate_text]
+
+    for _ in range(max_substitutions):
+        current_plates = possible_plates[:]
+
+        for plate in current_plates:
+            for i, char in enumerate(plate):
+                if char in substitutions:
+                    for replacement in substitutions[char]:
+                        new_plate = plate[:i] + replacement + plate[i+1:]
+                        possible_plates.append(new_plate)
+
+    return possible_plates
 
 def is_brazilian_license_plate(text):
     # Padrão para placas brasileiras:
@@ -94,28 +131,9 @@ def is_brazilian_license_plate(text):
 
     return bool(brazilian_plate_pattern_mercosul.match(text)) or bool(brazilian_plate_pattern_anterior.match(text))
 
-def extract_non_overlapping_boxes(image, boxes):
-    selected_boxes = []
-
-    for b in boxes.splitlines():
-        b = b.split()
-        x, y, w, h = int(b[1]), int(b[2]), int(b[3]), int(b[4])
-        print(f"box: x={x}; y={y}; w={w}; h={h}")
-        # Check if the current box is not overlapping with any selected box
-        if all(not is_overlap(box, (x, y, w, h)) for box in selected_boxes):
-            selected_boxes.append((x, y, w, h))
-
-    return selected_boxes
-
-def is_overlap(box1, box2):
-    x1, y1, w1, h1 = box1
-    x2, y2, w2, h2 = box2
-
-    return not (x1 + w1 < x2 or x2 + w2 < x1 or y1 + h1 < y2 or y2 + h2 < y1)
-
 def is_plate(image, contour):
     x, y, w, h = cv2.boundingRect(contour)                      #retângulo delimitador do contorno
-    
+
     if (h >= w) or (w < 100 and h < 30):
         return False
 
@@ -123,76 +141,37 @@ def is_plate(image, contour):
     if not (2.5 <= aspect_ratio <= 5.0):
         return False
 
-    # roi = image[y:y+h, x:x+w]                                   #recorta a região de interessa
-    # text = pytesseract.image_to_string(roi, config='--psm 6')   #tenta extrair o texto da imagem
-    # print(text)
-
     # OLD AQUI
     roi = image[y:y+h, x:x+w]                                   #recorta a região de interessa
     custom_config = r'--psm 6 -c tessedit_char_whitelist=ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789 --oem 3'
     result = pytesseract.image_to_string(roi, config=custom_config, output_type=pytesseract.Output.DICT)
     possible_plate = result['text'].strip()
     first_seven_characters = possible_plate[:7]
-
-    # IMPRIMIR OS QUADRADOS
-    h, w = roi.shape[:2]
-    boxes = pytesseract.image_to_boxes(roi, config=custom_config)
-
-    for b in boxes.splitlines():
-        b = b.split()
-        char_x, char_y, char_w, char_h = int(b[1]), h - int(b[2]), int(b[3]), h - int(b[4])
-        cv2.rectangle(roi, (char_x, char_y), (char_w, char_h), (0, 255, 0), 2)
-
-    # Display the image with bounding boxes
-    cv2.imshow('Character Boxes', roi)
-    cv2.waitKey(0)
-    cv2.destroyAllWindows()
     
-    # # NEW
-    # # Your code for extracting character boxes
-    # roi = image[y:y+h, x:x+w]
-    # custom_config = r'--psm 6 -c tessedit_char_whitelist=ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789 --oem 3'
+    # # IMPRIMIR OS QUADRADOS
+    # h, w = roi.shape[:2]
     # boxes = pytesseract.image_to_boxes(roi, config=custom_config)
 
-    # # Extract non-overlapping boxes
-    # selected_boxes = extract_non_overlapping_boxes(roi, boxes)
+    # for b in boxes.splitlines():
+    #     b = b.split()
+    #     char_x, char_y, char_w, char_h = int(b[1]), h - int(b[2]), int(b[3]), h - int(b[4])
+    #     cv2.rectangle(roi, (char_x, char_y), (char_w, char_h), (0, 255, 0), 2)
+
+    # # Display the image with bounding boxes
     # cv2.imshow('Character Boxes', roi)
     # cv2.waitKey(0)
     # cv2.destroyAllWindows()
-
-    # # Extract text from selected boxes
-    # for box in selected_boxes:
-    #     x, y, w, h = box
-    #     char_roi = roi[y:h, x:w]
-    #     result = pytesseract.image_to_string(char_roi, config=custom_config, output_type=pytesseract.Output.DICT)
-    #     possible_plate = result['text'].strip()
-    #     print(f"Text in box ({x}, {y}, {w}, {h}): {possible_plate}")
-
-    # # Display the image with non-overlapping bounding boxes
-    # for box in selected_boxes:
-    #     x, y, w, h = box
-    #     cv2.rectangle(roi, (x, y), (w, h), (0, 255, 0), 2)
-
-    # cv2.imshow('Character Boxes (Non-overlapping)', roi)
-    # cv2.waitKey(0)
-    # cv2.destroyAllWindows()
-    # print(possible_plate)
-    # return True
-
-    print(possible_plate)
-    print(f"first_seven_characters={first_seven_characters}")
-    if is_brazilian_license_plate(first_seven_characters):
-        print(first_seven_characters)
-        # print("É uma placa brasileira.")
-        return True
-    else:
-        # print("Não é uma placa brasileira.")
-        return False
-
-    if any(c.isalpha() or c.isdigit() for c in text):
-        return True
-    else:
-        return False
+    
+    if len(possible_plate) >= 7 and '\n' not in possible_plate:
+        print(f"Read from image={possible_plate}")
+        possible_plates = generate_possible_plates(possible_plate)
+        # print(f"Possible errors={possible_plates}")
+        final_plates = find_valid_plates(possible_plates)
+        print(f"\nPossible plates={final_plates}\n")
+        if final_plates:
+            return True
+    
+    return False
 
 def find_license_plate(img_original):
     image = pre_processing(img_original)    #pré-processamento da imagem   
@@ -207,6 +186,7 @@ def find_license_plate(img_original):
             x, y, w, h = cv2.boundingRect(contour) #desenha uma caixa delimitadora ao redor da placa
             cv2.rectangle(img_original, (x, y), (x + w, y + h), (0, 255, 0), 2)
             print("Placa detectada!")
+            break
 
     cv2.imshow("License Plate Detection", img_original)
     cv2.waitKey(0)
@@ -214,25 +194,3 @@ def find_license_plate(img_original):
 
 image = get_image()
 find_license_plate(image)
-
-
-"""def detect_license_plate(rectangle_text):
-    # Padrão de placa de veículo brasileira (exemplo, adapte conforme necessário)
-    license_plate_pattern = re.compile(r'^[A-Z]{3}\d{4}$')
-
-    # Verificar se o texto dentro do retângulo corresponde ao padrão da placa de veículo
-    match = license_plate_pattern.match(rectangle_text)
-
-    # Se houver correspondência, é uma placa de veículo
-    if match:
-        return True
-    else:
-        return False
-# Exemplo de uso
-rectangle_text = "ABC1234"
-result = detect_license_plate(rectangle_text)
-
-if result:
-    print("Placa de veículo detectada!")
-else:
-    print("Não é uma placa de veículo.")"""
