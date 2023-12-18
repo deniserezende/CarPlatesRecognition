fa7cf68c0b31a62f4d465b53e8243fcd5e0de515
Denise Rezende <denise.rezende@gmail.com>
2023-12-18 00:20:08 -0300

    Add images and edit code

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ import pytesseract
 def get_image():
     folder = 'conjunto-de-dados'
     absolute_path = os.path.abspath(folder)
-    png_files = [file for file in os.listdir(absolute_path) if file.endswith('.png') or file.endswith('.jpg') or file.endswith('.jpeg')]   #recuperando todos os arquivos .png da pasta conjunto-de-dados
+    png_files = [file for file in os.listdir(absolute_path) if file.endswith('.png') or file.endswith('.jpg') or file.endswith('.jpeg')]   # recuperando todos os arquivos imagens da pasta conjunto-de-dados
 
     print("\nConjunto de dados:")
     i = 0
@@ -18,28 +18,25 @@ def get_image():
         i += 1
 
     indice = input("\nDigite o índice da imagem que deseja carregar: ")
-    if indice == '.':
-        image = 'image-car-front.png'
-    else:
-        image = png_files[int(indice)]
+    image = png_files[int(indice)]
     image = cv2.imread(f'{folder}/{image}')
     
     return image
 
 def binary_quantize(image):
     q_image = image.copy()
-    q_image[q_image < 200] = 0      #define pixels mais escuros como preto
-    q_image[q_image >= 200] = 255   #define pixels mais claros como branco
+    q_image[q_image < 200] = 0      # define pixels mais escuros como preto
+    q_image[q_image >= 200] = 255   # define pixels mais claros como branco
 
     return q_image
 
 def pre_processing(imagem):
-    contraste = cv2.multiply(imagem, np.array([0.7]))    #ajuste de contraste
-    gray = cv2.cvtColor(contraste, cv2.COLOR_BGR2GRAY)   #escala de cinza
-    blur = cv2.GaussianBlur(gray, (3,3), 0)              #redução de ruídos
-    equalizada = cv2.equalizeHist(blur)                  #equalização de histograma (realce)
-    binary = binary_quantize(equalizada)                 #binarização
-    blur = cv2.GaussianBlur(binary, (3,3), 0)            #redução de ruídos
+    contraste = cv2.multiply(imagem, np.array([0.7]))    # ajuste de contraste
+    gray = cv2.cvtColor(contraste, cv2.COLOR_BGR2GRAY)   # escala de cinza
+    blur = cv2.GaussianBlur(gray, (3,3), 0)              # redução de ruídos
+    equalizada = cv2.equalizeHist(blur)                  # equalização de histograma (realce)
+    binary = binary_quantize(equalizada)                 # binarização
+    blur = cv2.GaussianBlur(binary, (3,3), 0)            # redução de ruídos
     cv2.imshow('Imagem tratada', blur)
     
     return blur
@@ -95,6 +92,7 @@ def find_valid_plates(possible_plates):
     return valid_plates
 
 def generate_possible_plates(plate_text, max_substitutions=2):
+    """Gera possíveis placas considerando erros comuns nas leituras"""
     substitutions = {
         '2': ['Z'],
         'Z': ['2'],
@@ -121,8 +119,9 @@ def generate_possible_plates(plate_text, max_substitutions=2):
     return possible_plates
 
 def is_plate(image, contour):
-    x, y, w, h = cv2.boundingRect(contour)                      #retângulo delimitador do contorno
+    x, y, w, h = cv2.boundingRect(contour)                      # retângulo delimitador do contorno
 
+    # Ignorando retangulos inválidos para placas
     if (h >= w) or (w < 100 and h < 30):
         return False
 
@@ -130,12 +129,11 @@ def is_plate(image, contour):
     if not (2.5 <= aspect_ratio <= 5.0):
         return False
 
-    # OLD AQUI
-    roi = image[y:y+h, x:x+w]                                   #recorta a região de interessa
+    # OCR
+    roi = image[y:y+h, x:x+w] # recorta a região de interessa
     custom_config = r'--psm 6 -c tessedit_char_whitelist=ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789 --oem 3'
     result = pytesseract.image_to_string(roi, config=custom_config, output_type=pytesseract.Output.DICT)
     possible_plate = result['text'].strip()
-    first_seven_characters = possible_plate[:7]
     
     if len(possible_plate) >= 7 and '\n' not in possible_plate:
         print(f"Read from image={possible_plate}")
@@ -145,20 +143,19 @@ def is_plate(image, contour):
         print(f"\nPossible plates={final_plates}\n")
         if final_plates:
             return True
-    
     return False
 
 def find_license_plate(img_original):
-    image = pre_processing(img_original)    #pré-processamento da imagem   
-    edges = cv2.Canny(image, 50, 150)       #detecção de bordas
-    contours, _ = cv2.findContours(edges, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE) #encontrando contornos na imagem
+    image = pre_processing(img_original)    # pré-processamento da imagem   
+    edges = cv2.Canny(image, 50, 150)       # detecção de bordas
+    contours, _ = cv2.findContours(edges, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE) # encontrando contornos na imagem
 
     for contour in contours:
         epsilon = 0.02 * cv2.arcLength(contour, True)
         approx = cv2.approxPolyDP(contour, epsilon, True)
 
         if is_plate(image, approx):
-            x, y, w, h = cv2.boundingRect(contour) #desenha uma caixa delimitadora ao redor da placa
+            x, y, w, h = cv2.boundingRect(contour) # desenha uma caixa delimitadora ao redor da placa
             cv2.rectangle(img_original, (x, y), (x + w, y + h), (0, 255, 0), 2)
             print("Placa detectada!")
             break
